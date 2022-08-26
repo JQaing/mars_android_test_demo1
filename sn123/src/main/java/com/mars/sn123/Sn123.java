d065c04450af5a4440dab882adb030811fe3039e
sinian.0608 <sinian.0608@bytedance.com>
2022-08-26 18:48:18 +0800

    add two component

---
+++ sn123/src/main/java/com/mars/sn123/Sn123.java
@@ -0,0 +1,9 @@
+package com.mars.sn123;
+
+public class Sn123 {
+
+    public static String tag() {
+        return "sn123 v3";
+    }
+
+}
---
+++ sn999/src/main/java/com/mars/sn999/Sn999.java
@@ -0,0 +1,9 @@
+package com.mars.sn999;
+
+public class Sn999 {
+
+    public static String tag() {
+        return "sn999 v3";
+    }
+
+}
